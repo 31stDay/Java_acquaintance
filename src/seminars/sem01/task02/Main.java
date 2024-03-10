17f2fc876bbee4a5101507cc678dda8adca04b8f
Dinochka <dina.dina-chernyshova@yandex.ru>
2024-03-10 17:35:25 +0300

    initial commit

---
+++ src/seminars/sem01/task02/Main.java
@@ -0,0 +1,30 @@
+package seminars.sem01.task02;
+
+/*
+Дан массив двоичных чисел, например [1,1,0,1,1,1],
+вывести максимальное количество подряд идущих 1.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,1,1};
+        int a = getCount(arr);
+        System.out.println(a);
+    }
+
+    static int getCount(int[] arr){
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1){
+                count++;
+                if (maxCount < count){
+                    maxCount = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
